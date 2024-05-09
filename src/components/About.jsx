5b3a3c00650a6e7f379e6b5a4516149271af9a5f
Hamza <hamzaaddami11@gmail.com>
2024-05-09 05:24:45 +0000

    Add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
             <div class="w-full flex justify-center">
               <div class="relative">
                 <img
-                  src='public/Pictures/MyPic.webp'
+                  src='public/Pictures/MyPic.png'
                   class="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px] max-h-[150px]"
                 />
               </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -96,7 +96,7 @@ const Hero = () => {
             alt="Hero"
             className="mx-auto aspect-square rounded-xl object-cover object-center overflow-hidden shad-bg-gray-200"
             height="400"
-            src="public/Pictures/PPP.webp"
+            src="public/Pictures/PPP.png"
             width="400"
           />
         </div>
